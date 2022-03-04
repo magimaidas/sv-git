86807f5ac636890ff286a3cfad18213dc01d1a58
magimaidas <reachmagimaidas@gmail.com>
2022-03-04 19:02:18 +0530

    ASN modified with purchase & Pantaq modules

--- odoo14/pantaq_asn/models/purchase_order.py
+++ odoo14/pantaq_asn/models/purchase_order.py
@@ -4,6 +4,7 @@ from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.tools.float_utils import float_is_zero
 from odoo.exceptions import AccessError, UserError, ValidationError
 from odoo.tools.misc import formatLang, get_lang
+from odoo.tools import float_compare, float_round, float_is_zero, OrderedSet
 
 
 class PurchaseOrder(models.Model):
@@ -12,8 +13,10 @@ class PurchaseOrder(models.Model):
     asn_ids = fields.One2many('purchase.asn', 'reference', string='ASN ids',
                               states={'cancel': [('readonly', True)], 'done': [('readonly', True)]}, copy=True)
     asn_count = fields.Integer(compute='_compute_asn', string='ASN count', default=0, store=True)
-    asn_done = fields.Boolean(string='ASN Done?', default=False, compute='_compute_asn_done',store=True)
-    rfq_ref = fields.Many2one('purchase.order',string="RFQ Reference")
+    asn_done = fields.Boolean(string='ASN Done?', default=False, compute='_compute_asn_done', store=True)
+    rfq_ref = fields.Many2one('purchase.order', string="RFQ Reference")
+    po_ref = fields.Many2one('purchase.order', string="PO Reference")
+
 
     @api.depends('asn_count')
     def _compute_asn_done(self):
@@ -27,11 +30,21 @@ class PurchaseOrder(models.Model):
     def button_confirm(self):
         for order in self:
 
-            if order.state not in ['draft', 'sent','qtn_received','rfq_revised']:
+            if order.state not in ['draft', 'sent', 'qtn_received', 'rfq_revised']:
                 continue
             if order.po_type == 'rfq':
                 new_order = order.copy()
-                new_order.rfq_ref = new_order.id
+                lines = new_order.order_line.filtered(lambda l: l.rfq_status == 'approved')
+                lines_remove = OrderedSet()
+                for line in order.order_line:
+                    if line.rfq_status == 'rejected':
+                        lines_remove.add(line.id)
+                res = self.env['purchase.order.line'].browse(lines_remove).sudo().unlink()
+                new_order.update({
+                    'state': 'done',
+                    'po_ref' : order.id,
+                })
+                order.rfq_ref = new_order.id
                 order.po_type = 'purchase'
                 order.name = self.env['ir.sequence'].next_by_code('purchase.order') or '/'
 
@@ -68,7 +81,7 @@ class PurchaseOrder(models.Model):
 
         result = self.env["ir.actions.actions"]._for_xml_id('pantaq_asn.action_asn_tree')
         asn_obj = self.env['purchase.asn'].search([('reference', '=', self.name)])
-        if len(asn_obj)==1:
+        if len(asn_obj) == 1:
             res = self.env.ref('pantaq_asn.pq_view_purchase_asn_form', False)
             form_view = [(res and res.id or False, 'form')]
             result['views'] = form_view
@@ -98,6 +111,7 @@ class PurchaseOrder(models.Model):
 
     def inspect_asn(self):
         print("Inspecting")
+
 
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
