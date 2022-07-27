5e5d65272432eea2cf5bc5b18d5efe03c0a28355
magimaidas <reachmagimaidas@gmail.com>
2022-07-27 15:06:00 +0530

    Updated

--- odoo12/pantaq/__manifest__.py
+++ odoo12/pantaq/__manifest__.py
@@ -3,35 +3,25 @@
     'name': "Pantaq",
     'summary': "Module created for Pantaq to handle Procurement process",
     'author': 'Systems Valley Ltd.,',
-    'license' : 'AGPL-3',
+    'license' : 'OPL-1',
+    'maintainer':'Systems Valley Pvt Ltd.,',
     'description': """
 Pantaq
 ====================
 Handles Logistics and Supply Chain process to aid Procurement specialists,
 who help companies achieve substantial cost savings on any area of spend.
 
-
     """,
     'website': 'https://www.pantaq.com',
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Logistics and Supply Chain',
     'version': '1.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base', 'crm', 'sale_crm', 'uom', 'purchase', 'hr', 'delivery_hs_code', 'stock', 'web'],
 
-    # always loaded
     'data': [
         'data/data.xml',
-
         'security/security.xml',
         'security/ir.model.access.csv',
-
         'data/crm_stage_data.xml',
-
         'data/email_templates.xml',
         'data/procurement_email_templates.xml',
         'wizard/wizard_view.xml',
@@ -45,8 +35,6 @@ who help companies achieve substantial cost savings on any area of spend.
         'views/templates.xml',
         'views/account_invoice_view.xml',
         'views/product_template_views.xml',
-        # 'views/enquiry_assets.xml',
-
         'report/report_layout.xml',
         'report/purchase_order_templates.xml',
         'report/purchase_quotation_templates.xml'
@@ -55,5 +43,5 @@ who help companies achieve substantial cost savings on any area of spend.
     'application': True,
     'auto_install': False,
     'uninstall_hook': 'delete_group',
-    'sequence': 1,
+    'sequence':1,
 }
